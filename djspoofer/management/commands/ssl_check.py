b0f51fd7a2c5cc8580a2ca07dc3c0673bdb3cad4
Adrian <adrian@rydeas.com>
2022-04-04 12:36:31 -0500

    moved commands to djspoofer app

--- djspoofer/management/commands/ssl_check.py
+++ djspoofer/management/commands/ssl_check.py
@@ -2,9 +2,7 @@ from django.core.management.base import BaseCommand
 from djstarter import utils
 
 from djspoofer.clients import DesktopChromeClient
-from djspoofer.models import Fingerprint
 from howsmyssl import howsmyssl_api
-from intoli.models import Profile
 
 
 class Command(BaseCommand):
@@ -12,7 +10,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         try:
-            with DesktopChromeClient(self.get_fingerprint()) as chrome_client:
+            with DesktopChromeClient() as chrome_client:
                 r_check = howsmyssl_api.ssl_check(chrome_client)
                 self.stdout.write(utils.pretty_dict(r_check.data))
         except Exception as e:
@@ -20,17 +18,3 @@ class Command(BaseCommand):
             raise e
         else:
             self.stdout.write(self.style.MIGRATE_LABEL(f'Successfully got ssl check details'))
-
-    @staticmethod
-    def get_fingerprint():
-        profile = Profile.objects.weighted_desktop_profile()
-        return Fingerprint(
-            device_category=profile.device_category,
-            platform=profile.os,
-            screen_height=profile.screen_height,
-            screen_width=profile.screen_width,
-            user_agent=profile.data,
-            viewport_height=profile.viewport_height,
-            viewport_width=profile.viewport_width,
-            # proxy=proxy
-        )
