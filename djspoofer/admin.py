d497cd3b4222d315889b4a41cc9dfae09e573f1c
Adrian <adrian@rydeas.com>
2022-04-07 22:45:40 -0500

    updated admin models to subclass ImportExportModelAdmin

--- djspoofer/admin.py
+++ djspoofer/admin.py
@@ -1,15 +1,15 @@
 from django.conf.locale.en import formats as en_formats
 from django.contrib import admin
-from djstarter import admin as core_admin
+from import_export.admin import ImportExportModelAdmin
 
-from . import const
+from . import const, resources
 from .models import Fingerprint, Proxy
 
 en_formats.DATETIME_FORMAT = "M d y H:i"
 
 
 @admin.register(Fingerprint)
-class FingerprintAdmin(core_admin.BaseAdmin):
+class FingerprintAdmin(ImportExportModelAdmin):
     list_display = ['created', 'device_category', 'platform', 'user_agent']
     list_filter = ('device_category', 'platform',)
     ordering = ['-created']
@@ -17,23 +17,16 @@ class FingerprintAdmin(core_admin.BaseAdmin):
 
     show_full_result_count = False
 
-    def bulk_delete(self, request, queryset):
-        queryset.delete()
-
-    bulk_delete.short_description = 'Bulk Delete'
-
-    actions = ['export_as_csv', bulk_delete]
-
 
 @admin.register(Proxy)
-class ProxyAdmin(core_admin.BaseAdmin):
+class ProxyAdmin(ImportExportModelAdmin):
     list_display = ['url', 'mode', 'cooldown', 'last_used', 'used_count', 'country', 'city']
     list_filter = ('mode', 'country', 'city')
     ordering = ['mode', 'url']
     search_fields = ['url']
 
     show_full_result_count = False
-    read_only_exclude = ('url', 'mode', 'country')
+    resource_class = resources.ProxyAdminResource
 
     def set_rotating(self, request, queryset):
         queryset.update(mode=const.ProxyModes.ROTATING.value)
@@ -50,9 +43,4 @@ class ProxyAdmin(core_admin.BaseAdmin):
 
     set_general.short_description = 'Set as General'
 
-    def bulk_delete(self, request, queryset):
-        queryset.delete()
-
-    bulk_delete.short_description = 'Bulk Delete'
-
-    actions = ['export_as_csv', set_rotating, set_sticky, set_general, bulk_delete]
+    actions = [set_rotating, set_sticky, set_general]
