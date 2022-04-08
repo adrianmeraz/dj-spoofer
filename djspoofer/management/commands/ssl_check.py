5996e600b5e3e11a744795293e7b9ef1202fb1bf
Adrian <adrian@rydeas.com>
2022-04-08 14:51:30 -0500

    added better exception handling for proxy manager

--- djspoofer/management/commands/ssl_check.py
+++ djspoofer/management/commands/ssl_check.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from djstarter import utils
 
+from djspoofer.models import Proxy
 from djspoofer.clients import DesktopChromeClient
 from howsmyssl import howsmyssl_api
 
@@ -8,7 +9,16 @@ from howsmyssl import howsmyssl_api
 class Command(BaseCommand):
     help = 'SSL Check'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--use-proxy",
+            required=True,
+            type=bool,
+            help="Use Random Proxy",
+        )
+
     def handle(self, *args, **kwargs):
+        use_proxy = kwargs['use_proxy']
         try:
             with DesktopChromeClient() as chrome_client:
                 r_check = howsmyssl_api.ssl_check(chrome_client)
