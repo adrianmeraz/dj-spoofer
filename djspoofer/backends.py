6ae61ac5661f197c5e04d1ebd162a082d3d640dd
adrian <adrian@rydeas.com>
2022-04-25 20:24:34 -0500

    excluded from coverage

--- djspoofer/backends.py
+++ djspoofer/backends.py
@@ -7,10 +7,10 @@ logger = logging.getLogger(__name__)
 class ProxyBackend(ABC):
 
     def get_proxy_url(self, ip_fingerprint):
-        raise NotImplemented
+        raise NotImplemented    # pragma: no cover
 
     def is_valid_proxy(self, proxies):
-        raise NotImplemented
+        raise NotImplemented    # pragma: no cover
 
     def new_ip_fingerprint(self, fingerprint):
-        raise NotImplemented
+        raise NotImplemented    # pragma: no cover
