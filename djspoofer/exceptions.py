f9188c555e879e02131a35c615ed3b104c211842
adrian <adrian@rydeas.com>
2022-05-09 23:19:08 -0500

    failed proxy connections now retry

--- djspoofer/exceptions.py
+++ djspoofer/exceptions.py
@@ -3,3 +3,7 @@ from djstarter import exceptions
 
 class DJSpooferError(exceptions.AppError):
     """All Exceptions in the Spoofer App inherit from this class"""
+
+
+class ProxyConnectionFailed(DJSpooferError):
+    """For any failed proxy connections"""
